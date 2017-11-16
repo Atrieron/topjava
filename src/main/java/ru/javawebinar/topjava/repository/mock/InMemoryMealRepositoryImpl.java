9b556fcade87b2a26aa86b0e79e22fe0bf93fcf7
Atrieron <31343512+Nmptc@users.noreply.github.com>
2017-11-16 10:19:51 +0400

    "Rollback"

--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
@@ -21,11 +21,9 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
     public Meal save(Meal meal) {
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
-            return meal;
         }
-        // treat case: update, but absent in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        repository.put(meal.getId(), meal);
+        return meal;
     }
 
     @Override
